05208fb3ac9b52b42aaca4562fe642b564dbf986
Abe <field.abe.d@gmail.com>
2025-07-12 11:13:54 -0600

    verified SparkJava web server responds on /hello

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,16 @@
 import chess.*;
+import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        port(8080);
+
+        get("/hello", (req, res) -> {
+            res.type("application/json");
+            return "{\"message\": \"Hello, Chess CS240!\"}";
+        });
     }
 }
