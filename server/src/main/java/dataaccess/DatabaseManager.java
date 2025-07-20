1a0fd3c948642471cb38321b822d2f3f8088ca0c
Abe <field.abe.d@gmail.com>
2025-07-19 23:37:34 -0600

    Should be fixed on the non local db connection

--- server/src/main/java/dataaccess/DatabaseManager.java
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -7,7 +7,8 @@ public class DatabaseManager {
     private static String databaseName;
     private static String dbUsername;
     private static String dbPassword;
-    private static String connectionUrl;
+    private static String baseConnectionUrl;
+    private static String dbConnectionUrl;
 
     // Table creation statements
     private static final String[] createStatements = {
@@ -51,19 +52,13 @@ public class DatabaseManager {
         """
     };
 
-    /*
-     * Load the database information for the db.properties file.
-     */
     static {
         loadPropertiesFromResources();
     }
 
-    /**
-     * Creates the database if it does not already exist.
-     */
     static public void createDatabase() throws DataAccessException {
         var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
-        try (var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+        try (var conn = DriverManager.getConnection(baseConnectionUrl, dbUsername, dbPassword);
              var preparedStatement = conn.prepareStatement(statement)) {
             preparedStatement.executeUpdate();
         } catch (SQLException ex) {
@@ -71,10 +66,6 @@ public class DatabaseManager {
         }
     }
 
-    /**
-     * Initializes all tables if they do not exist.
-     * Call this once at server startup.
-     */
     public static void initializeTables() throws DataAccessException {
         createDatabase();
         try (var conn = getConnection()) {
@@ -88,23 +79,9 @@ public class DatabaseManager {
         }
     }
 
-    /**
-     * Create a connection to the database and sets the catalog based upon the
-     * properties specified in db.properties. Connections to the database should
-     * be short-lived, and you must close the connection when you are done with it.
-     * The easiest way to do that is with a try-with-resource block.
-     * <br/>
-     * <code>
-     * try (var conn = DatabaseManager.getConnection()) {
-     * // execute SQL statements.
-     * }
-     * </code>
-     */
     public static Connection getConnection() throws DataAccessException {
         try {
-            //do not wrap the following line with a try-with-resources
-            var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
-            conn.setCatalog(databaseName);
+            var conn = DriverManager.getConnection(dbConnectionUrl, dbUsername, dbPassword);
             return conn;
         } catch (SQLException ex) {
             throw new DataAccessException("failed to get connection", ex);
@@ -128,9 +105,9 @@ public class DatabaseManager {
         databaseName = props.getProperty("db.name");
         dbUsername = props.getProperty("db.user");
         dbPassword = props.getProperty("db.password");
-
         var host = props.getProperty("db.host");
         var port = Integer.parseInt(props.getProperty("db.port"));
-        connectionUrl = String.format("jdbc:mysql://%s:%d/%s", host, port, databaseName);
+        baseConnectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
+        dbConnectionUrl = String.format("jdbc:mysql://%s:%d/%s", host, port, databaseName);
     }
 }
